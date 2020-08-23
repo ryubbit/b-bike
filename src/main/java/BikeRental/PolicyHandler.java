eb83676d95b8512ba0c37b44f9863a43413b3caf
unknown <Administrator@SKCC16D00064.SKCC.NET>
2020-08-23 21:21:37 +0900

    2nd commit

--- src/main/java/BikeRental/PolicyHandler.java
+++ src/main/java/BikeRental/PolicyHandler.java
@@ -24,7 +24,6 @@ public class PolicyHandler{
         if(rentalCancelled.isMe()){
             System.out.println("##### listener RentalCancel : " + rentalCancelled.toJson());
 
-            // Listener를 통해서 호출되는 경우는 Cancel
             Bike bike = new Bike();
             bike.setId(rentalCancelled.getBikeId());
             bike.setStatus("Available");
